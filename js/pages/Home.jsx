c9e41f5c8c09e79df10d45a51bf35086dd156336
Michael Mann <michael.mann55@gmail.com>
2021-08-25 10:25:14 +0300

    4

---
+++ js/pages/Home.jsx
@@ -0,0 +1,13 @@
+export function Home() {
+    return (
+      <section className='app-home'>
+        <h1>Welcome</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Itaque et nihil quis ducimus cum, ex, quo odit voluptatem
+          enim aspernatur perspiciatis illum ipsum voluptas
+          accusantium, tempore delectus neque cupiditate qui!
+        </p>
+      </section>
+    );
+  }
