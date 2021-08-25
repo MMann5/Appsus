78a10fca70ea2fba74b1352dffd51c6148be03d8
KotiMango <rons20000@gmail.com>
2021-08-25 10:58:56 +0300

    Crafted Boilerplate for appsus

---
+++ js/apps/keep/pages/NoteApp.jsx
@@ -0,0 +1,1 @@
+export class NoteApp extends React.Component {}
---
+++ js/apps/mail/services/mail.service.js
@@ -0,0 +1,21 @@
+const email = {
+  id: 'e101',
+  subject: 'Miss you!',
+  body: 'Would love to catch up sometimes',
+  isRead: false,
+  sentAt: 1551133930594,
+  to: 'momo@momo.com',
+};
+
+const loggedinUser = {
+  email: 'user@appsus.com',
+  fullname: 'Mahatma Appsus',
+};
+
+const criteria = {
+  status: 'inbox/sent/trash/draft',
+  txt: 'puki',
+  isRead: true,
+  isStared: true,
+  lables: ['important', 'romantic'],
+};
