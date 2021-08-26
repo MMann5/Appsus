48948b699cec52c650c058879c947c9c1c9b577b
Michael Mann <michael.mann55@gmail.com>
2021-08-26 14:24:43 +0300

    css repare

--- js/apps/keep/cmps/NotePreview.jsx
+++ js/apps/keep/cmps/NotePreview.jsx
@@ -3,7 +3,7 @@ export function NotePreview({ note, onDeleteNote, onChangeColor, onUpdateNote,on
         <div className="note-preview" style={{ backgroundColor: note.style.backgroundColor }}>
             <textarea onChange={(ev) => {
                 onUpdateNote(ev, note.id)
-            }} className="filter-txt" id="" cols="35" rows="4" style={{ backgroundColor: note.style.backgroundColor }}>{note.info.txt}</textarea>
+            }} className="filter-txt" id="" cols="30" rows="4" style={{ backgroundColor: note.style.backgroundColor }}>{note.info.txt}</textarea>
             <img className='img-preview' src={note.info.url} />
             <div className="note-btn">
                 <button className="delete" onClick={() => { onDeleteNote(note.id) }}>X</button>
