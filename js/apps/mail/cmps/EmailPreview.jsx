b608a828d1131404ff04d18fe5d8d12ade6b1af9
Michael Mann <michael.mann55@gmail.com>
2021-08-27 12:19:32 +0300

    css mail

--- js/apps/mail/cmps/EmailPreview.jsx
+++ js/apps/mail/cmps/EmailPreview.jsx
@@ -18,10 +18,11 @@ export function EmailPreview({
           >
             <td className="obj">{email.subject}</td>
             <td className="body" >{email.body}</td>
+            {/* <td className="mailto">{email.to}</td> */}
           </div>
 
           <td className="star obj"><input type='checkbox' className='email-starred' onChange={(ev) => onToggleStar(ev, email.id)} /></td>
-          <td className="obj"><button className="fa fa-trash fa-2x" onClick={() => onDeleteEmail(email.id)}></button></td>
+          <td><button className="fa fa-trash fa-2x" onClick={() => onDeleteEmail(email.id)}></button></td>
         </tr>
         </tbody>
       </table>
